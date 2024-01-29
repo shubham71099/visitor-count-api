f2f28c39afb3fd91d605930d4bdcf01bfba71a5b
“shubham71099” <shubham71099@gmail.com>
2024-01-29 21:05:14 +0530

    added middleware

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: process.env.ORIGINS.split(" "),
